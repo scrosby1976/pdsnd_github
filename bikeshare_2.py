3f6cee81dfec307c67bd56c8241c7e7249a8c82a
root <root@a31992ec4da1>
2023-01-19 11:42:05 +0000

    docs: under def get_fileters(): added 'and' between new york and washington

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -33,7 +33,7 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
 
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # TO DO: get user input for city (chicago, new york city, and washington). HINT: Use a while loop to handle invalid inputs
 
     cities = ['chicago', 'new york city', 'washington']
 
